c0cbb0387685f7d6cf0e75b13172ab7cbe87e86c
Kamran <huseynovkamran07@gmail.com>
2022-09-26 11:03:10 +0400

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,59 @@
+#***#
+
+#input
+# name = input(" what is your name? ")
+# print("Hello " + name)
+
+#***#
+
+#variables
+
+#birth_year = input("enter your birth year:")
+#age = 2022 - int(birth_year)
+#print (age)
+
+#***#
+
+#calculator
+
+#first_number = input("İlk rəqəm: ")
+#second_number = input("İkinci reqem: ")
+#sum = float(first_number) + float(second_number)
+#print("cavab: ",sum)
+#print("cavab: " str(sum))
+#***#
+
+#strings
+
+#course = 'python for beginners'
+#print ('python' in course )
+#print(course.replace('for', '4'))
+#print = (course.find('Y'))
+#print(course.upper())
+
+#***#
+
+#Arithmetic Operators
+
+#print (10 ** 3)
+#x= 10
+#x= x+12
+#print (x)
+
+###
+
+#price = 25
+#print (price > 10 and price <30)
+
+#if_else statement
+
+#temperature = int(input("Temperaturu daxil edin: "))
+#if temperature>30:
+#    print("Bol bol su için")
+#elif temperature >10:
+#    print("Biraz soyuqdur")
+#elif temperature >20:
+#    print("Gözəl bir gündür!")
+#else:
+#    print("Qalin geyinin")
+
